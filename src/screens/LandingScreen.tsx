6ed1f0a87bf8263ddedd4693855ce5b1670a9a6d
karimLa <kira00luffy@gmail.com>
2020-12-09 22:02:48 +0100

    initialized project

---
+++ src/screens/LandingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const LandingScreen = () => {
+	return (
+		<View>
+			<Text>choose your language</Text>
+		</View>
+	);
+};
+
+export default LandingScreen;
+
+const styles = StyleSheet.create({});
